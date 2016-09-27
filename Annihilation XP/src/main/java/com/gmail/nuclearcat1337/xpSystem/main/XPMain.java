be17ab97add4bbcc21b1ae739a905d02becf1462
kh498 <klink100@gmail.com>
2016-09-28 00:42:26 +0200

    Finished xp to gold

--- Annihilation XP/src/main/java/com/gmail/nuclearcat1337/xpSystem/main/XPMain.java
+++ Annihilation XP/src/main/java/com/gmail/nuclearcat1337/xpSystem/main/XPMain.java
@@ -28,7 +28,6 @@ import com.gmail.nuclearcat1337.xpSystem.shop.Shop;
 import com.gmail.nuclearcat1337.xpSystem.xp.MyXPCommand;
 import com.gmail.nuclearcat1337.xpSystem.xp.XPListeners;
 import com.gmail.nuclearcat1337.xpSystem.xp.XPSystem;
-import com.hcs.anniPro.playerParty.PartyCommands;
 import com.hcs.convertXP.Convert;
 
 public final class XPMain extends JavaPlugin implements Listener
@@ -37,218 +36,217 @@ public final class XPMain extends JavaPlugin implements Listener
 	private YamlConfiguration config;
 	private File configFile;
 	private static List<Perm> perms;
-	
-	private final String CHAT_PREFIX = "[" + this.getName() + "] ";
 
-	@Override
-	public void onEnable()
+	private final String CHAT_PREFIX = "[" + this.getName () + "] ";
+
+	@ Override
+	public void onEnable ()
 	{
-		configFile = new File(AnnihilationMain.getInstance().getDataFolder(),"AnnihilationXPConfig.yml");
-		Bukkit.getLogger().info(CHAT_PREFIX + "Loading XP system...");
-		Bukkit.getPluginManager().registerEvents(this, this);
+		configFile = new File (AnnihilationMain.getInstance ().getDataFolder (), "AnnihilationXPConfig.yml");
+		Bukkit.getLogger ().info (CHAT_PREFIX + "Loading XP system...");
+		Bukkit.getPluginManager ().registerEvents (this, this);
 
-		checkFile(configFile);
-		config = YamlConfiguration.loadConfiguration(configFile);
-		
+		checkFile (configFile);
+		config = YamlConfiguration.loadConfiguration (configFile);
+
 		//%# to be replaced by the number
 		int x = 0;
-		x += ConfigManager.setDefaultIfNotSet(config, "Nexus-Hit-XP", 1);
-		x += ConfigManager.setDefaultIfNotSet(config, "Player-Kill-XP", 3);
-		x += ConfigManager.setDefaultIfNotSet(config, "Winning-Team-XP", 100);
-		x += ConfigManager.setDefaultIfNotSet(config, "Second-Place-Team-XP", 75);
-		x += ConfigManager.setDefaultIfNotSet(config, "Third-Place-Team-XP", 50);
-		x += ConfigManager.setDefaultIfNotSet(config, "Last-Place-Team-XP", 25);
-		x += ConfigManager.setDefaultIfNotSet(config, "Gave-XP-Message", "&a+%# Annihilation XP");
-		x += ConfigManager.setDefaultIfNotSet(config, "MyXP-Command-Message", "&dYou have &a%#&d Annihilation XP.");
-		if(!config.isConfigurationSection("XP-Multipliers"))
+		x += ConfigManager.setDefaultIfNotSet (config, "Nexus-Hit-XP", 1);
+		x += ConfigManager.setDefaultIfNotSet (config, "Player-Kill-XP", 3);
+		x += ConfigManager.setDefaultIfNotSet (config, "Winning-Team-XP", 100);
+		x += ConfigManager.setDefaultIfNotSet (config, "Second-Place-Team-XP", 75);
+		x += ConfigManager.setDefaultIfNotSet (config, "Third-Place-Team-XP", 50);
+		x += ConfigManager.setDefaultIfNotSet (config, "Last-Place-Team-XP", 25);
+		x += ConfigManager.setDefaultIfNotSet (config, "Gave-XP-Message", "&a+%# Annihilation XP");
+		x += ConfigManager.setDefaultIfNotSet (config, "MyXP-Command-Message", "&dYou have &a%#&d Annihilation XP.");
+		if (!config.isConfigurationSection ("XP-Multipliers"))
 		{
-			ConfigurationSection multipliers = config.createSection("XP-Multipliers");
-			multipliers.set("Multiplier-1.Permission", "Anni.XP.2");
-			multipliers.set("Multiplier-1.Multiplier", 2);
+			ConfigurationSection multipliers = config.createSection ("XP-Multipliers");
+			multipliers.set ("Multiplier-1.Permission", "Anni.XP.2");
+			multipliers.set ("Multiplier-1.Multiplier", 2);
 			x++;
 		}
-		
-		ConfigurationSection data = config.getConfigurationSection("Database");
-		if(data == null)
-			data = config.createSection("Database");
-		
-		x += setupDatebaseConfig(data);
-		
-		ConfigurationSection shopSec = config.getConfigurationSection("Kit-Shop");
-		if(shopSec == null)
+
+		ConfigurationSection data = config.getConfigurationSection ("Database");
+		if (data == null)
+			data = config.createSection ("Database");
+
+		x += setupDatebaseConfig (data);
+
+		ConfigurationSection shopSec = config.getConfigurationSection ("Kit-Shop");
+		if (shopSec == null)
 		{
-			shopSec = config.createSection("Kit-Shop");
-			shopSec.createSection("Kits");
+			shopSec = config.createSection ("Kit-Shop");
+			shopSec.createSection ("Kits");
 		}
-		
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "On", false);
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "Already-Purchased-Kit", "&aPURCHASED");
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "Not-Yet-Purchased-Kit", "&cLOCKED. PURCHASE FOR &6%# &cXP");
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "Confirm-Purchase-Kit", "&aPUCHASE BEGUN. CONFIRM FOR &6%# &AXP");
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "Confirmation-Expired", "&cThe confirmation time has expired. Please try again.");
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "Not-Enough-XP", "&cYou do not have enough XP to purchase this kit.");
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "Kit-Purchased", "&aKit %w purchased!");
-		x += ConfigManager.setDefaultIfNotSet(shopSec, "No-Kits-To-Purchase", "&cNo kits left to purchase!");
-		
-		
-		if(x > 0)
-			this.saveConfig();
-		
-		XPMain.xpSystem = new XPSystem(config.getConfigurationSection("Database"));
-		if(!XPMain.xpSystem.isActive())
+
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "On", false);
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "Already-Purchased-Kit", "&aPURCHASED");
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "Not-Yet-Purchased-Kit", "&cLOCKED. PURCHASE FOR &6%# &cXP");
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "Confirm-Purchase-Kit", "&aPUCHASE BEGUN. CONFIRM FOR &6%# &AXP");
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "Confirmation-Expired", "&cThe confirmation time has expired. Please try again.");
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "Not-Enough-XP", "&cYou do not have enough XP to purchase this kit.");
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "Kit-Purchased", "&aKit %w purchased!");
+		x += ConfigManager.setDefaultIfNotSet (shopSec, "No-Kits-To-Purchase", "&cNo kits left to purchase!");
+
+		if (x > 0)
+			this.saveConfig ();
+
+		XPMain.xpSystem = new XPSystem (config.getConfigurationSection ("Database"));
+		if (!XPMain.xpSystem.isActive ())
 		{
-			Bukkit.getLogger().info(CHAT_PREFIX + "Could NOT connect to the XP database");
-			disable();
+			Bukkit.getLogger ().info (CHAT_PREFIX + "Could NOT connect to the XP database");
+			disable ();
 			return;
 		}
-		Bukkit.getLogger().info(CHAT_PREFIX + "CONNECTED to XP database");
-		boolean useShop = config.getBoolean("Kit-Shop.On");
-		if(useShop)
+		Bukkit.getLogger ().info (CHAT_PREFIX + "CONNECTED to XP database");
+		boolean useShop = config.getBoolean ("Kit-Shop.On");
+		if (useShop)
 		{
-			Bukkit.getLogger().info(CHAT_PREFIX + "The shop is ENABLED");
-			this.getCommand("Shop").setExecutor(new Shop(XPMain.xpSystem,config.getConfigurationSection("Kit-Shop"), this));
-			saveConfig();
-		}
-		else Bukkit.getLogger().info(CHAT_PREFIX + "The shop is DISABLED");
-		loadMultipliers(config.getConfigurationSection("XP-Multipliers"));
-		loadXPVars(config); //This also loads the listeners with the values they need
-		AnniCommand.registerArgument(new XPArgument(xpSystem));
-		AnniCommand.registerArgument(new KitArgument(xpSystem));
-		for(AnniPlayer p : AnniPlayer.getPlayers())
-			xpSystem.loadKits(p, null);
-		
+			Bukkit.getLogger ().info (CHAT_PREFIX + "The shop is ENABLED");
+			this.getCommand ("Shop").setExecutor (new Shop (XPMain.xpSystem, config.getConfigurationSection ("Kit-Shop"), this));
+			saveConfig ();
+		} else
+			Bukkit.getLogger ().info (CHAT_PREFIX + "The shop is DISABLED");
+		loadMultipliers (config.getConfigurationSection ("XP-Multipliers"));
+		loadXPVars (config); //This also loads the listeners with the values they need
+		AnniCommand.registerArgument (new XPArgument (xpSystem));
+		AnniCommand.registerArgument (new KitArgument (xpSystem));
+		for (AnniPlayer p : AnniPlayer.getPlayers ())
+			xpSystem.loadKits (p, null);
+
 		//seting up converting to gold
-		new Convert(this);
+		new Convert (this, xpSystem);
 	}
-	
-	private int setupDatebaseConfig(ConfigurationSection data){
+
+	private int setupDatebaseConfig (ConfigurationSection data)
+	{
 		int x = 0;
-		x += ConfigManager.setDefaultIfNotSet(data, "Host", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Port", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Database", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Username", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Password", "Test");
+		x += ConfigManager.setDefaultIfNotSet (data, "Host", "Test");
+		x += ConfigManager.setDefaultIfNotSet (data, "Port", "Test");
+		x += ConfigManager.setDefaultIfNotSet (data, "Database", "Test");
+		x += ConfigManager.setDefaultIfNotSet (data, "Username", "Test");
+		x += ConfigManager.setDefaultIfNotSet (data, "Password", "Test");
 		return x;
 	}
-	
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void loadKits(PlayerJoinEvent e)
+
+	@ EventHandler (priority = EventPriority.MONITOR)
+	public void loadKits (PlayerJoinEvent e)
 	{
-		AnniPlayer p = AnniPlayer.getPlayer(e.getPlayer().getUniqueId());
-		if(p != null)
-			xpSystem.loadKits(p, null);
+		AnniPlayer p = AnniPlayer.getPlayer (e.getPlayer ().getUniqueId ());
+		if (p != null)
+			xpSystem.loadKits (p, null);
 	}
-	
-	private void checkFile(File file)
+
+	private void checkFile (File file)
 	{
-		if(!file.exists())
+		if (!file.exists ())
 		{
 			try
 			{
-				file.createNewFile();
-			}
-			catch (IOException e)
+				file.createNewFile ();
+			} catch (IOException e)
 			{
-				e.printStackTrace();
+				e.printStackTrace ();
 			}
 		}
 	}
-	
-	public void saveConfig()
+
+	@ Override
+	public void saveConfig ()
 	{
 		try
 		{
-			config.save(configFile);
-		}
-		catch (IOException e)
+			config.save (configFile);
+		} catch (IOException e)
 		{
-			e.printStackTrace();
+			e.printStackTrace ();
 		}
 	}
-	
-	private void disable()
+
+	private void disable ()
 	{
-		detachAllDatabases();
-		Bukkit.getLogger().info(CHAT_PREFIX + "Disabling XP System.");
-		Bukkit.getPluginManager().disablePlugin(this);
+		detachAllDatabases ();
+		Bukkit.getLogger ().info (CHAT_PREFIX + "Disabling XP System.");
+		Bukkit.getPluginManager ().disablePlugin (this);
 	}
-	
-	public void loadXPVars(ConfigurationSection section)
+
+	public void loadXPVars (ConfigurationSection section)
 	{
 		assert section != null;
 
-		int nexusHitXP = section.getInt("Nexus-Hit-XP");
-		int killXP = section.getInt("Player-Kill-XP");
-		String gaveXPMessage = section.getString("Gave-XP-Message");
-		String myXPMessage = section.getString("MyXP-Command-Message");
+		int nexusHitXP = section.getInt ("Nexus-Hit-XP");
+		int killXP = section.getInt ("Player-Kill-XP");
+		String gaveXPMessage = section.getString ("Gave-XP-Message");
+		String myXPMessage = section.getString ("MyXP-Command-Message");
 		int[] teamXPs = new int[4];
-		teamXPs[0] = section.getInt("Winning-Team-XP");
-		teamXPs[1] = section.getInt("Second-Place-Team-XP");
-		teamXPs[2] = section.getInt("Third-Place-Team-XP");
-		teamXPs[3] = section.getInt("Last-Place-Team-XP");
-		
-		XPListeners listeners = new XPListeners(XPMain.xpSystem,gaveXPMessage,killXP,nexusHitXP,teamXPs);
-		
-		Bukkit.getPluginManager().registerEvents(listeners, this);
-		
-		MyXPCommand xpCommand = new MyXPCommand(XPMain.xpSystem,myXPMessage);
+		teamXPs[0] = section.getInt ("Winning-Team-XP");
+		teamXPs[1] = section.getInt ("Second-Place-Team-XP");
+		teamXPs[2] = section.getInt ("Third-Place-Team-XP");
+		teamXPs[3] = section.getInt ("Last-Place-Team-XP");
 
-		this.getCommand("MyXP").setExecutor(xpCommand);
+		XPListeners listeners = new XPListeners (XPMain.xpSystem, gaveXPMessage, killXP, nexusHitXP, teamXPs);
+
+		Bukkit.getPluginManager ().registerEvents (listeners, this);
+
+		MyXPCommand xpCommand = new MyXPCommand (XPMain.xpSystem, myXPMessage);
+
+		this.getCommand ("MyXP").setExecutor (xpCommand);
 
 	}
-	
-	public void loadMultipliers(ConfigurationSection multipliers)
+
+	public void loadMultipliers (ConfigurationSection multipliers)
 	{
 		assert multipliers != null;
-		perms = new ArrayList<Perm>();
+		perms = new ArrayList<Perm> ();
 		//ConfigurationSection multipliers = config.getConfigurationSection("XP-Multipliers");
-		if(multipliers != null)
+		if (multipliers != null)
 		{
-			for(String key : multipliers.getKeys(false))
+			for (String key : multipliers.getKeys (false))
 			{
-				ConfigurationSection multSec = multipliers.getConfigurationSection(key);
-				String perm = multSec.getString("Permission");
-				int multiplier = multSec.getInt("Multiplier");
-				if(perm != null && !perm.equals("") && multiplier > 0)
+				ConfigurationSection multSec = multipliers.getConfigurationSection (key);
+				String perm = multSec.getString ("Permission");
+				int multiplier = multSec.getInt ("Multiplier");
+				if (perm != null && !perm.equals ("") && multiplier > 0)
 				{
-					Permission p = new Permission(perm);
-					p.setDefault(PermissionDefault.FALSE);
-					Bukkit.getPluginManager().addPermission(p);
-					p.recalculatePermissibles();
-					perms.add(new Perm(perm,multiplier));			
+					Permission p = new Permission (perm);
+					p.setDefault (PermissionDefault.FALSE);
+					Bukkit.getPluginManager ().addPermission (p);
+					p.recalculatePermissibles ();
+					perms.add (new Perm (perm, multiplier));
 				}
 			}
-			Collections.sort(perms);
+			Collections.sort (perms);
 		}
 	}
-	
 
-	@Override
-	public void onDisable()
+	@ Override
+	public void onDisable ()
 	{
-		detachAllDatabases();
+		detachAllDatabases ();
 	}
-	
-	public void detachAllDatabases(){
-		if(xpSystem != null)
-			xpSystem.disable();
-	}
-	
-	public static String formatString(String string, int amount)
+
+	public void detachAllDatabases ()
 	{
-		return ChatColor.translateAlternateColorCodes('&', string.replace("%#", ""+amount));
+		if (xpSystem != null)
+			xpSystem.disable ();
 	}
-	
-	public static int checkMultipliers(Player player, int initialXP)
+
+	public static String formatString (String string, int amount)
+	{
+		return ChatColor.translateAlternateColorCodes ('&', string.replace ("%#", "" + amount));
+	}
+
+	public static int checkMultipliers (Player player, int initialXP)
 	{
 		int returnXP = initialXP;
-		if(perms.size() > 0)
+		if (perms.size () > 0)
 		{
-			for(Perm p : perms)
+			for (Perm p : perms)
 			{
-				if(player.hasPermission(p.perm))
+				if (player.hasPermission (p.perm))
 				{
-					returnXP = (int)Math.ceil(((double)returnXP)*p.multiplier);
+					returnXP = (int) Math.ceil ((returnXP) * p.multiplier);
 					break;
 				}
 			}
@@ -256,7 +254,7 @@ public final class XPMain extends JavaPlugin implements Listener
 		return returnXP;
 	}
 
-	public static XPSystem getXpSystem()
+	public static XPSystem getXpSystem ()
 	{
 		return xpSystem;
 	}
--- Annihilation XP/src/main/java/com/hcs/convertXP/Convert.java
+++ Annihilation XP/src/main/java/com/hcs/convertXP/Convert.java
@@ -1,11 +1,12 @@
 package com.hcs.convertXP;
 
+import java.util.Iterator;
+
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.command.PluginCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
@@ -17,35 +18,74 @@ import com.gmail.nuclearcat1337.anniPro.itemMenus.ItemClickHandler;
 import com.gmail.nuclearcat1337.anniPro.itemMenus.ItemMenu;
 import com.gmail.nuclearcat1337.anniPro.itemMenus.ItemMenu.Size;
 import com.gmail.nuclearcat1337.anniPro.itemMenus.MenuItem;
+import com.gmail.nuclearcat1337.xpSystem.utils.Acceptor;
+import com.gmail.nuclearcat1337.xpSystem.xp.XPSystem;
 
+/**
+ * Converts annihilation xp to in game gold
+ * 
+ * @author kh498
+ */
 public class Convert implements CommandExecutor
 {
 	//	private Plugin plugin;
 	private static String CMD = "convert";
+	private static XPSystem xpSystem;
 
-	public Convert(JavaPlugin plugin)
+	public Convert (JavaPlugin plugin, XPSystem system)
 	{
-		plugin.getCommand(CMD).setExecutor(this);
+		plugin.getCommand (CMD).setExecutor (this);
+		Convert.xpSystem = system;
 		//		this.plugin = plugin;
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
+	@ Override
+	public boolean onCommand (CommandSender sender, Command command, String label, String[] args)
 	{
 		if (sender instanceof Player)
 		{
 			Player player = (Player) sender;
+			//display gui when there is no arguments
 			if (args.length == 0)
 			{
-				openExchanger(player);
+				openExchanger (player);
 				return true;
 			}
-			for (Exchange e : Exchange.values())
+			//check if one of the arguments match the enums
+			for (Exchange e : Exchange.values ())
 			{
-				if (args[0].equalsIgnoreCase(e.name()))
+				if (args[0].equalsIgnoreCase (e.name ()))
 				{
-					player.sendMessage(e.name() + " Sucess!");
-					return true;
+					if (e.hasEnoughSpace (player))
+					{
+						if (xpSystem.isActive ())
+						{
+							//Get the player's xp  (this takes some times as we are speaking to an external db)
+							xpSystem.getXP (player.getUniqueId (), new Acceptor<Integer> ()
+							{
+								@ Override
+								public void accept (Integer amount)
+								{
+
+									if (amount >= e.getXpCost ())
+									{
+										player.getInventory ().addItem (e.getItem ());
+										xpSystem.removeXP (player.getUniqueId (), e.getXpCost ());
+										player.sendMessage (ChatColor.GREEN + "You successfully bought " + e.getItemName () + "!");
+									} else
+									{
+										player.sendMessage (
+												ChatColor.RED + "You do not have enough Annihilation xp to buy " + e.getItemName () + ".");
+									}
+								}
+							});
+						}
+
+					} else
+					{
+						player.sendMessage (ChatColor.RED + "You do not have the space to buy " + e.getItemName () + ".");
+					}
+					break;
 				}
 			}
 
@@ -53,28 +93,38 @@ public class Convert implements CommandExecutor
 		return true;
 	}
 
-	public void openExchanger(Player player)
+	/**
+	 * Opens a shop GUI to the player
+	 * 
+	 * @param player
+	 *            The player that will see the gui
+	 */
+	public void openExchanger (Player player)
 	{
-		player.sendMessage(""+Exchange.LARGE.returnItem.getAmount());
-		ItemMenu menu = new ItemMenu("exchange xp for gold", Size.ONE_LINE);
-		
-		menu.setItem(0, Exchange.SMALL.toMenuItem());
-		menu.setItem(4, Exchange.MEDIUM.toMenuItem());
-		menu.setItem(8, Exchange.LARGE.toMenuItem());
-		menu.open(player);
+		ItemMenu menu = new ItemMenu ("Gold Shop", Size.ONE_LINE);
+
+		menu.setItem (0, Exchange.SMALL.toMenuItem ());
+		menu.setItem (4, Exchange.MEDIUM.toMenuItem ());
+		menu.setItem (8, Exchange.LARGE.toMenuItem ());
+		menu.open (player);
 	}
 
+	/**
+	 * All possible exchanges that can be made from Anni xp to gold (as item)
+	 * 
+	 * @author kh498
+	 */
 	enum Exchange
 	{
-		SMALL(1000, new ItemStack(Material.GOLD_INGOT, 64), Material.GOLD_INGOT),
-		MEDIUM(10000, new ItemStack(Material.GOLD_BLOCK, 80), Material.GOLD_BLOCK),
-		LARGE(25000, new ItemStack(Material.GOLD_BLOCK, 225), Material.DIAMOND_BLOCK);
-		
-		private int xpCost;
-		private ItemStack returnItem;
-		private Material icon;
+		SMALL(1000, new ItemStack (Material.GOLD_INGOT, 64), Material.GOLD_INGOT),
+		MEDIUM(10000, new ItemStack (Material.GOLD_BLOCK, 80), Material.GOLD_BLOCK),
+		LARGE(25000, new ItemStack (Material.GOLD_BLOCK, 224), Material.DIAMOND_BLOCK);
 
-		Exchange(int xp, ItemStack item, Material icon)
+		private final int xpCost;
+		private final ItemStack returnItem;
+		private final Material icon;
+
+		Exchange (int xp, ItemStack item, Material icon)
 		{
 			this.xpCost = xp;
 			this.returnItem = item;
@@ -82,31 +132,114 @@ public class Convert implements CommandExecutor
 		}
 
 		/**
-		 * First letter to uppercase rest is lowercase
+		 * @return <tt>this.name()</tt> with the first case uppercase
 		 */
-		@Override
-		public String toString()
+		@ Override
+		public String toString ()
 		{
-			return this.name().substring(0, 1).toUpperCase() + this.name().substring(1, this.name().length()).toLowerCase();
+			return this.name ().substring (0, 1).toUpperCase () + this.name ().substring (1, this.name ().length ()).toLowerCase ();
 		}
 
-		public MenuItem toMenuItem()
+		/**
+		 * @return The MenuItem of the exchange item
+		 */
+		public MenuItem toMenuItem ()
 		{
-			String name = this.toString();
-			return (new ActionMenuItem(ChatColor.AQUA + this.toString() + " Package", new ItemClickHandler()
+			String name = this.toString ();
+			String[] lore = new String[] { ChatColor.GRAY + "Buy " + this.getItemName () + " for " + this.getXpCost () + " xp" };
+			//			lore[0] = "Buy " + this.getItemName () + " for " + this.getXpCost () + " xp";
+
+			return (new ActionMenuItem (ChatColor.AQUA + this.toString () + " Package", new ItemClickHandler ()
 			{
-				@Override
-				public void onItemClick(ItemClickEvent event)
+				@ Override
+				public void onItemClick (ItemClickEvent event)
 				{
-					event.getPlayer().performCommand(CMD + " " + name);
-					event.setWillClose(true);
+					event.getPlayer ().performCommand (CMD + " " + name);
+					event.setWillClose (true);
+
 				}
-			}, new ItemStack(icon), new String[] {}));
+			}, new ItemStack (icon), lore));
 		}
-		
-		public boolean canTrade(Player player){
-			final Inventory inv = player.getInventory();			
+
+		/**
+		 * Check if a player have enough xp to trade
+		 * 
+		 * @param player
+		 *            The player to check
+		 * @return True if the amount of xp needed is equal to less than the player's xp
+		 */
+		public boolean hasEnoughXp (Player player)
+		{
+
 			return false;
+		}
+
+		/**
+		 * Check if a player have enough xp to trade.
+		 * 
+		 * @param player
+		 *            The player to check
+		 * @return True if the player can trade
+		 */
+		public boolean hasEnoughSpace (Player player)
+		{
+			final Inventory inv = player.getInventory ();
+			final ItemStack item = this.returnItem;
+
+			//There is an empty slot and you can fit the whole item in the slot
+			if (inv.firstEmpty () != -1 && item.getAmount () <= item.getMaxStackSize ())
+			{
+				return true;
+			}
+
+			int nrOfEmptySlots = 0;
+			for (Iterator<ItemStack> i = inv.iterator (); i.hasNext ();)
+			{
+				if (i.next () == null)
+				{
+					nrOfEmptySlots++;
+				}
+			}
+
+			if (nrOfEmptySlots >= Math.ceil (item.getAmount () / item.getMaxStackSize ()))
+			{
+				return true;
+			}
+			return false;
+		}
+
+		/**
+		 * @return The amount of xp it costs
+		 */
+		public int getXpCost ()
+		{
+			return xpCost;
+		}
+
+		/**
+		 * @return The item to return upon a sucsessfull trade
+		 */
+		public ItemStack getItem ()
+		{
+			return returnItem;
+		}
+
+		/**
+		 * @return A propper name for the return item
+		 */
+		public String getItemName ()
+		{
+			return this.getItem ().getAmount () + " " + this.getItem ().getType ().toString ().replace ("_", " ").toLowerCase () + "s";
+			//			return name + (this.getItem ().getAmount () > 1) != null
+			//					? ("s").equalsIgnoreCase (name.substring (name.length () - 1, name.length ())) ? "'" : "'s" : "";
+		}
+
+		/**
+		 * @return The item to return upon a sucsessfull trade
+		 */
+		public Material getIcon ()
+		{
+			return icon;
 		}
 
 	}
