add340a057988b0ceae45de74ca29c09d3ac5c6b
kh498 <klink100@gmail.com>
2016-09-29 09:28:24 +0200

    AA will now use XP's database

--- Annihilation Achievements/src/main/java/com/gmail/nuclearcat1337/kitSystem/main/KitMain.java
+++ Annihilation Achievements/src/main/java/com/gmail/nuclearcat1337/kitSystem/main/KitMain.java
@@ -14,7 +14,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import com.gmail.nuclearcat1337.anniPro.main.AnniCommand;
 import com.gmail.nuclearcat1337.anniPro.main.AnnihilationMain;
-import com.gmail.nuclearcat1337.anniPro.voting.ConfigManager;
 import com.gmail.nuclearcat1337.kitSystem.achievementViewer.AchievementViewer;
 import com.gmail.nuclearcat1337.kitSystem.kitAchievement.KitListeners;
 import com.gmail.nuclearcat1337.kitSystem.kitAchievement.KitSystem;
@@ -25,137 +24,118 @@ public final class KitMain extends JavaPlugin implements Listener
 	private KitSystem kitSystem;
 	private YamlConfiguration config;
 	private File configFile;
-	private final String CHAT_PREFIX = "[" + this.getName() + "] ";
+	private final String CHAT_PREFIX = "[" + this.getName () + "] ";
 
-	@Override
-	public void onEnable()
+	@ Override
+	public void onEnable ()
 	{
-		configFile = new File(AnnihilationMain.getInstance().getDataFolder(),"AnnihilationAchivementConfig.yml");
-		Bukkit.getLogger().info(CHAT_PREFIX + "Loading Kit Achievement system...");
-		Bukkit.getPluginManager().registerEvents(this, this);
+		//we're just using the same config as anniXP
+		configFile = new File (AnnihilationMain.getInstance ().getDataFolder (), "AnnihilationXPConfig.yml");
+		Bukkit.getLogger ().info (CHAT_PREFIX + "Loading Kit Achievement system...");
+		Bukkit.getPluginManager ().registerEvents (this, this);
 
-		checkFile(configFile);
-		config = YamlConfiguration.loadConfiguration(configFile);
-		
-		//%# to be replaced by the number
-		int x = 0;
-		
-		ConfigurationSection kits = config.getConfigurationSection("Database");
-		if(kits == null)
-			kits = config.createSection("Database");
-		
-		x += setupDatebaseConfig(kits);
-		
-		
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "On", false);
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "Already-Purchased-Kit", "&aPURCHASED");
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "Not-Yet-Purchased-Kit", "&cLOCKED. PURCHASE FOR &6%# &cXP");
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "Confirm-Purchase-Kit", "&aPUCHASE BEGUN. CONFIRM FOR &6%# &AXP");
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "Confirmation-Expired", "&cThe confirmation time has expired. Please try again.");
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "Not-Enough-XP", "&cYou do not have enough XP to purchase this kit.");
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "Kit-Purchased", "&aKit %w purchased!");
-//		x += ConfigManager.setDefaultIfNotSet(shopSec, "No-Kits-To-Purchase", "&cNo kits left to purchase!");
-		
-		//save the config if any changes have been detected
-		if(x > 0)
-			this.saveConfig();
-		
-		try {
-			this.kitSystem = new KitSystem(config.getConfigurationSection("Database"), XPMain.getXpSystem());
-		} catch (NoClassDefFoundError e){ //something went wrong with initlizing anni xp
-			Bukkit.getLogger().info(CHAT_PREFIX + "Could NOT connect to the kit achievement database, due to dependency of Annihilation XP");
-			disable(); //We cannot function without the xp addon
+		checkFile (configFile);
+		config = YamlConfiguration.loadConfiguration (configFile);
+
+		try
+		{
+			this.kitSystem = new KitSystem (config.getConfigurationSection ("Database"), XPMain.getXpSystem ());
+		} catch (NoClassDefFoundError e)
+		{ //something went wrong with initlizing anni xp
+			Bukkit.getLogger ().info (CHAT_PREFIX + "Could NOT connect to the kit achievement database, due to dependency of Annihilation XP");
+			disable (); //We cannot function without the xp addon
 			return;
 		}
-		Bukkit.getLogger().info(CHAT_PREFIX + "CONNECTED to kit database");
-		
-		loadKitVars(config); //This also loads the listeners with the values they need
-		
-		AnniCommand.registerArgument(new KitAchivementArgument(kitSystem));
+		Bukkit.getLogger ().info (CHAT_PREFIX + "CONNECTED to kit database");
+
+		loadKitVars (config); //This also loads the listeners with the values they need
+
+		AnniCommand.registerArgument (new KitAchivementArgument (kitSystem));
 	}
-	
-	public void loadKitVars(ConfigurationSection section)
+
+	public void loadKitVars (ConfigurationSection section)
 	{
 		assert section != null;
-		
-		KitListeners listeners = new KitListeners(kitSystem);
-		
-		Bukkit.getPluginManager().registerEvents(listeners, this);
-		
-		AchievementViewer kitCommand = new AchievementViewer(kitSystem);
 
-		this.getCommand("Achievements").setExecutor(kitCommand);
-		this.getCommand("Achievements").setAliases(new ArrayList<>(Arrays.asList("Ach")));
+		KitListeners listeners = new KitListeners (kitSystem);
+
+		Bukkit.getPluginManager ().registerEvents (listeners, this);
+
+		AchievementViewer kitCommand = new AchievementViewer (kitSystem);
+
+		this.getCommand ("Achievements").setExecutor (kitCommand);
+		this.getCommand ("Achievements").setAliases (new ArrayList<> (Arrays.asList ("Ach")));
 	}
-	
-	private int setupDatebaseConfig(ConfigurationSection data){
-		int x = 0;
-		x += ConfigManager.setDefaultIfNotSet(data, "Host", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Port", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Database", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Username", "Test");
-		x += ConfigManager.setDefaultIfNotSet(data, "Password", "Test");
-		return x;
-	}
-	
-	private void checkFile(File file)
+
+	//	private int setupDatebaseConfig (ConfigurationSection data)
+	//	{
+	//		int x = 0;
+	//		x += ConfigManager.setDefaultIfNotSet (data, "Host", "Test");
+	//		x += ConfigManager.setDefaultIfNotSet (data, "Port", "Test");
+	//		x += ConfigManager.setDefaultIfNotSet (data, "Database", "Test");
+	//		x += ConfigManager.setDefaultIfNotSet (data, "Username", "Test");
+	//		x += ConfigManager.setDefaultIfNotSet (data, "Password", "Test");
+	//		return x;
+	//	}
+
+	private void checkFile (File file)
 	{
-		if(!file.exists())
+		if (!file.exists ())
 		{
 			try
 			{
-				file.createNewFile();
-			}
-			catch (IOException e)
+				file.createNewFile ();
+			} catch (IOException e)
 			{
-				e.printStackTrace();
+				e.printStackTrace ();
 			}
 		}
 	}
-	
-	public void saveConfig()
+
+	@ Override
+	public void saveConfig ()
 	{
 		try
 		{
-			config.save(configFile);
-		}
-		catch (IOException e)
+			config.save (configFile);
+		} catch (IOException e)
 		{
-			e.printStackTrace();
+			e.printStackTrace ();
 		}
 	}
-	
-	private void disable()
-	{
-		detachAllDatabases();
-		Bukkit.getLogger().info(CHAT_PREFIX + "Disabling Kit System.");
-		Bukkit.getPluginManager().disablePlugin(this);
-	}
-	
 
-	@Override
-	public void onDisable()
+	private void disable ()
 	{
-		detachAllDatabases();
+		detachAllDatabases ();
+		Bukkit.getLogger ().info (CHAT_PREFIX + "Disabling Kit System.");
+		Bukkit.getPluginManager ().disablePlugin (this);
 	}
-	
-	public void detachAllDatabases(){
-		if (kitSystem != null){
-			kitSystem.disable();
+
+	@ Override
+	public void onDisable ()
+	{
+		detachAllDatabases ();
+	}
+
+	public void detachAllDatabases ()
+	{
+		if (kitSystem != null)
+		{
+			kitSystem.disable ();
 		}
 	}
-	
-	public static String formatString(String string, int amount)
+
+	public static String formatString (String string, int amount)
 	{
-		return ChatColor.translateAlternateColorCodes('&', string.replace("%#", ""+amount));
+		return ChatColor.translateAlternateColorCodes ('&', string.replace ("%#", "" + amount));
 	}
-	
-	public static String[] toStringArray(String s)
+
+	public static String[] toStringArray (String s)
 	{
-		if (s.contains("%n"))
-			return s.split("%n");
+		if (s.contains ("%n"))
+			return s.split ("%n");
 		else
 			return new String[] { s };
 	}
-	
+
 }
